714efd2d9b60373a2030fa3f347e3e07656cc372
ChamsolPark <osoryo@naver.com>
2020-06-26 00:23:17 +0900

    Issue #(N+2) ~#M+2: 예외 처리 try-except문 이용  예외 처리에 대한 방법 또 다른 방법 중 하나인 try-except 문을 이용하였다.valueerror 를 사용함으로 서 예외 처리에 대한 능률을 올릴 수 있었다.

--- calc/calc.py
+++ calc/calc.py
@@ -6,11 +6,11 @@ def application(environ, start_response):
     first_num = d.get('first_num',[''])[0]
     second_num = d.get('second_num', [''])[0]
     sum, mul = -1, -1
-    if first_num.isdigit() and second_num.isdigit(): 
+    try:
         first_num, second_num = int(first_num), int(second_num)
         sum = first_num + second_num
         mul = first_num * second_num
-    else:
+    except ValueError:
         sum = -99999999        
         mul = -99999999
     response_body = html % {'sum':sum, 'mul':mul}
